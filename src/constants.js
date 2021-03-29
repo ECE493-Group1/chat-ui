582bc0c4ab373146a5222c58a988a81f850b32ea
Cody Gramlich <cmgramli@ualberta.ca>
2021-03-29 12:16:31 -0600

    Add user service url and routes to constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 export const CHAT_BACKEND_URL = "http://localhost:8000"
 export const CHAT_BACKEND_ROOMS = "/rooms"
+
+export const USER_SERVICE_URL = "http://localhost:3000"
+export const USER_SERVICE_REGISTER = "/register"
+export const USER_SERVICE_LOGIN = "/login"
+export const USER_SERVICE_USER_SEARCH = "/user-search"
