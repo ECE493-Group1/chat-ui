790068d0665dba0cfcd67c89befe851b9270813a
ken t <tellambu@ualberta.ca>
2021-04-04 13:24:01 -0600

    improved thread feed

--- src/views/ThreadFeed.vue
+++ src/views/ThreadFeed.vue
@@ -3,10 +3,28 @@
     <v-row>
       <v-col cols="12">
         <v-list>
-          <v-list-item v-for="(room, i) in rooms" :key="i" @click="pickroom(room.id)">
-            <v-list-item-title>Name: {{ room.name }}</v-list-item-title>
-            <v-list-item-subtitle>Id: {{ room.id }}</v-list-item-subtitle>
+          <v-list-item v-for="(room, i) in rooms" :key="i">
+            <v-list-item-content>
+              <v-list-item-title class="text-h4">{{
+                room.title
+              }}</v-list-item-title>
+              <v-list-item-subtitle class="text-h6 font-italic">
+                {{room.lastMessage}}
+              </v-list-item-subtitle>
+              <v-list-item-subtitle
+                >Members: {{ room.members.join(", ") }}</v-list-item-subtitle
+              >
+            </v-list-item-content>
+            <v-list-item-action>
+              <v-list-item-action-text>{{
+                shortTime(room.lastMessageTime)
+              }}</v-list-item-action-text>
+              <v-btn plain @click="pickroom(room.roomId)">
+                <v-icon x-large> mdi-arrow-right-thick </v-icon>
+              </v-btn>
+            </v-list-item-action>
           </v-list-item>
+          <v-divider></v-divider>
         </v-list>
       </v-col>
     </v-row>
@@ -15,7 +33,7 @@
 
 <script>
 import axios from "axios";
-import { CHAT_BACKEND_ROOMS, CHAT_BACKEND_URL } from "../constants"
+import { CHAT_BACKEND_ROOMS, CHAT_BACKEND_URL } from "../constants";
 
 export default {
   name: "ThreadFeed",
@@ -24,17 +42,26 @@ export default {
     rooms: [],
   }),
   methods: {
-    pickroom: function(roomId) {
-      this.$router.push("/chat/" + roomId)
-    }
+    pickroom: function (roomId) {
+      this.$router.push("/chat/" + roomId);
+    },
+    shortTime: function (messageTime) {
+      var currentTime = Math.floor(Date.now()/1000);
+      var difference = currentTime - messageTime;
+      if (difference < 60) {
+        return difference.toString() + " seconds ago";
+      }
+      if (difference < 3600) {
+        return Math.ceil(difference / 3600).toString() + " minutes ago";
+      }
+      if (difference < 86400) {
+        return Math.ceil(difference / 86400).toString + " days ago";
+      }
+    },
   },
   mounted() {
     axios.get(CHAT_BACKEND_URL + CHAT_BACKEND_ROOMS).then((response) => {
-      var names = response.data.rooms;
-      var ids = response.data.ids;
-      this.rooms = names.map(function (e, i) {
-        return { "name": e, "id": ids[i] };
-      });
+      this.rooms = response.data.rooms;
     });
   },
 };
